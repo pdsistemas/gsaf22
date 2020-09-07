e73b268675232b238dc5a25bff4fa1933623db20
pdsistemas <pdsistemas88@gmail.com>
2020-09-07 15:11:45 -0300

    consigna

---
+++ TP1.Panaderia.py
@@ -0,0 +1,36 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Patricio D'Antonio
+#
+# Created:     24/10/2019
+# Copyright:   (c) Patricio D'Antonio 2019
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+#!/usr/bin/env python
+
+
+
+# Hacer un programa que calcule el precio a pagar de cada cliente de una panaderia
+# y la cantidad de kg vendidos en el día.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
